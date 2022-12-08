4c00e1366be3fea7a79fee86ec3f2da0312e8003
Jorge <jorgediamanto01@gmail.com>
2022-12-09 00:44:57 +0100

    dasibvasfibpvdfs

---
+++ Ej1/operaciones.py
@@ -0,0 +1,45 @@
+def suma(num1,num2):
+    a = isinstance(num1, int)
+    b = isinstance(num2, int)
+    
+    if not a or not b :
+        raise TypeError("Variable no es un número")
+
+    x = num1+num2
+    
+    return x
+
+def resta(num1,num2):
+    a = isinstance(num1, int)
+    b = isinstance(num2, int)
+    if not a or not b ==False:
+        raise TypeError("Variable no es un número")
+
+    x = num1-num2
+    return x
+
+def producto(num1,num2):
+    a = isinstance(num1, int)
+    b = isinstance(num2, int)
+    if not a or not b ==False:
+        raise TypeError("Variable no es un número")
+
+    x = num1*num2
+    return x
+
+    
+
+def división(num1,num2):
+    a = isinstance(num1, int)
+    b = isinstance(num2, int)
+    if not a or not b ==False:
+        raise TypeError("Variable no es un número")
+
+    if num2==0:
+        raise ZeroDivisionError("Error al intentar dividir por 0")
+    
+    else:
+        x=num1/num2
+        return x
+
+
