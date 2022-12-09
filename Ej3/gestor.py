33133bfa9d249c89c3393297df2d6d22ca94269a
Jorge <jorgediamanto01@gmail.com>
2022-12-09 02:18:32 +0100

    dsa

--- Ej3/gestor.py
+++ Ej3/gestor.py
@@ -1,3 +1,5 @@
+import random
+
 class Personaje:
 
     def __init__(self,nombre ,vida, ataque, defensa, alcance):
@@ -19,14 +21,45 @@ class Personaje:
             raise ValueError("alcance debe ser mayor que 0")
         self.alcance = alcance
 
+    def __str__(self):
+        return "nombre: "+self.nombre+" vida: "+str(self.vida)+" ataque: "+str(self.ataque)+" defensa: "+str(self.defensa)+" alcance: "+str(self.alcance)
+
+    def __delete__(self):
+        del self.value
+
 
 class Gestor:
 
     def añadir():
-        pass
+        nombre = input("Introducir nombre: ")
+        b = random.randint(1,10)
+        c = random.randint(1,10)
+        d = random.randint(1,10)
+        e = random.randint(1,10)
+        a = Personaje(nombre,b,c,d,e)
 
-    def borrar():
-        pass
+    
 
-    def mostrar():
-        pass
+    def borrar(self):
+        del self
+
+    def mostrar(self):
+        print(self)
+
+a = Personaje("Caballero",4,2,4,2)
+b = Personaje("Guerrero",2,4,2,4)
+c = Personaje("Arquero",2,4,1,8)
+
+
+
+a.mostrar()
+b.mostrar()
+c.mostrar()
+
+c.borrar()
+
+
+a.mostrar()
+b.mostrar()
+
+
