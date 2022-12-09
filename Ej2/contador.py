8fa715e1ffde2df2a38fc6ab36466159fc22ace3
Jorge <jorgediamanto01@gmail.com>
2022-12-09 01:28:26 +0100

    SSSSSSSSSSSS

--- Ej2/contador.py
+++ Ej2/contador.py
@@ -1,5 +1,43 @@
 f = open('contador.txt', 'r')
-squares, cubes, fourths = [], [], []
+
 for line in f.readlines():
     print(line)
+
 f.close()
+
+while True:
+    x = input("inc -para incrementar , dec - para disminuir: ")
+
+    if x == "inc":
+        f = open('contador.txt', 'r')
+
+        for line in f.readlines():
+            xx = line
+            
+        
+        open('contador.txt', 'w').close()
+        with open('contador.txt', 'w') as file:
+            file.write(str(int(xx)+1))
+
+        f = open('contador.txt', 'r')
+        for line in f.readlines():
+            print(line)
+        f.close()
+
+    elif x == "dec":
+        f = open('contador.txt', 'r')
+
+        for line in f.readlines():
+            xx = line
+
+        open('contador.txt', 'w').close()
+        with open('contador.txt', 'w') as file:
+            file.write(str(int(xx)-1))
+
+        f = open('contador.txt', 'r')
+        for line in f.readlines():
+            print(line)
+        f.close()
+
+    else:
+        break
