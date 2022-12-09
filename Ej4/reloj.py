8c340cad252cb47d12a939f344dcd9bc847bff4f
Jorge <jorgediamanto01@gmail.com>
2022-12-09 02:38:56 +0100

    finit

--- Ej4/reloj.py
+++ Ej4/reloj.py
@@ -1,12 +1,21 @@
 import datetime
+import os
+
+
 
 now = datetime.datetime.now()
-print("Hora actual es: ")
+
 print(now.strftime("%H:%M:%S"))
 x=now.strftime("%H:%M:%S")
 
 
 while True:
+    now = datetime.datetime.now()
+    
     if x!=now.strftime("%H:%M:%S"):
+        os.system('cls')
+        print("Hora actual es: ")
         print(now.strftime("%H:%M:%S"))
         x=now.strftime("%H:%M:%S")
+        
+
